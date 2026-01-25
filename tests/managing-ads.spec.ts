e9000b8d3e4dc624a422b6dfd780bad88cb61cac
Geradot <toshkageradot@tuta.io>
2026-01-25 16:43:06 +0100

    delete mock test

--- tests/managing-ads.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "../fixtures/fixtures";
-
-test.describe("Create and delete ads", () => {
-    test("First test", async ({ createUnitPageWithAds, page }) => {
-        console.log("Test is done!");
-    });
-});
